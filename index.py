cd797b5e0d2b6d112a66b4bf244ed68c770d38c9
Russell Jurney <russell.jurney@gmail.com>
2012-09-18 14:37:16 -0700

    Made simple flask app

--- index.py
+++ index.py
@@ -1,3 +1,15 @@
 from flask import Flask
 import pycassa
+import json
 
+pool = pycassa.ConnectionPool('enron')
+cf = pycassa.ColumnFamily(pool, 'email_topics')
+
+app = Flask(__name__)
+
+@app.route("/message/topics/<message_id>")
+def topics(message_id):
+  return json.dumps(cf.get(message_id))
+
+if __name__ == "__main__":
+  app.run(debug=True)
